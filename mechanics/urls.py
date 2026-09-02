84b56eb2fdf6b8f9f5b46cde96d683626b1654b1
Mayuri <mayuripaliwal8@gmail.com>
2026-09-02 08:12:02 +0530

    feat: add /mechanic/{id} API

--- mechanics/urls.py
+++ mechanics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MechanicListView
+from .views import MechanicListView, MechanicDetailView
 
 urlpatterns = [
-    path('mechanics/',MechanicListView.as_view())
+    path('mechanics/',MechanicListView.as_view()),
+    path('mechanics/<int:id>/',MechanicDetailView.as_view())
 ]
--- mechanics/views.py
+++ mechanics/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.views import APIView
 from .models import Mechanic
 from .serializers import MechanicSerializer
@@ -6,9 +6,22 @@ from rest_framework.response import Response
 
 # Create your views here.
 class MechanicListView(APIView):
+    #this api returns all mechanics
     def get(self,request):
         mechanics=Mechanic.objects.all()
 
         serializer=MechanicSerializer(mechanics,many=True)
 
         return Response(serializer.data)
+
+class MechanicDetailView(APIView):
+    #this api returns mechanic for a given id
+    def get(self,request,id):
+        # if exist, return mechanic for the given id
+        # else return 404
+        mechanic=get_object_or_404(Mechanic,id=id)
+        serializer=MechanicSerializer(mechanic)
+
+        return Response(serializer.data)
+
+    
