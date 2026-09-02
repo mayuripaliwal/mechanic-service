9ad27a21a092c6bece02b793277368ec1cf037a9
Mayuri <mayuripaliwal8@gmail.com>
2026-09-02 11:17:31 +0530

    feat: add service request validation

--- mechanics/serializers.py
+++ mechanics/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Mechanic
 from .models import ServiceRequest
+import re
 
 # Serializer for mechanic model
 class MechanicSerializer(serializers.ModelSerializer):
@@ -12,7 +13,11 @@ class MechanicSerializer(serializers.ModelSerializer):
 class ServiceRequestSerializer(serializers.ModelSerializer):
     mechanic_id=serializers.PrimaryKeyRelatedField(
         queryset=Mechanic.objects.all(),
-        source="mechanic"
+        source="mechanic",
+        #error message if mechanic with the given id does not exist
+        error_messages={
+            "does_not_exist":"Mechanic with the given id does not exist."
+        }
     )
 
     class Meta:
@@ -28,3 +33,33 @@ class ServiceRequestSerializer(serializers.ModelSerializer):
             "status",
             "created_at"
         ]
+
+    #handle invalid customer phone number
+    def validate_customer_phone(self,value):
+        if len(value)!=10 or not value.isdigit():
+            raise serializers.ValidationError("Customer phone number must be a 10-digit number.")
+
+        return value
+
+    #handle invalid vehicle number
+    def validate_vehicle_number(self,value):
+        pattern=r"^[A-Z]{2}\d{1,2}[A-Z]{1,3}\d{4}$"
+        if not re.match(pattern,value):
+            raise serializers.ValidationError("Vehicle number must be in the format: XX00XX0000 (e.g., MH12AB1234).")
+
+        return value
+
+    #handle if this service not provided by mechanic
+    def validate(self,data):
+        mechanic=data["mechanic"]
+        service=data["service"]
+
+        #check if this service is provided by mechanic
+        if service not in mechanic.services:
+            raise serializers.ValidationError({
+                "service": "This service is not provided by the selected mechanic."
+            })
+
+        return data
+
+    
